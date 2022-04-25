c19e90b9ea5b889a6727cf68ef5ca59d684f36a2
brent-a <brent.k.aronsen@emcins.com>
2022-04-25 12:07:33 -0500

    Finish section 6 - OOP part 1

--- src/com/brentaronsen/section6OOP1/Section6Main.java
+++ src/com/brentaronsen/section6OOP1/Section6Main.java
@@ -5,11 +5,21 @@ public class Section6Main {
     public static void section6Main() {
 
 
-        Animal animal = new Animal("Animal", 1, 1, 5, 5);
-        Dog dog = new Dog("Yorkie", 8, 20, 2, 4, 1, 20, "long silky");
-        dog.eat();
-        dog.walk();
-        dog.run();
+        //Exercise 35: Cylinder
+//        Circle circle = new Circle(3.75);
+//        System.out.println("circle.radius= " + circle.getRadius());
+//        System.out.println("circle.area= " + circle.getArea());
+//        Cylinder cylinder = new Cylinder(5.55, 7.25);
+//        System.out.println("cylinder.radius= " + cylinder.getRadius());
+//        System.out.println("cylinder.height= " + cylinder.getHeight());
+//        System.out.println("cylinder.area= " + cylinder.getArea());
+//        System.out.println("cylinder.volume= " + cylinder.getVolume());
+
+//        Animal animal = new Animal("Animal", 1, 1, 5, 5);
+//        Dog dog = new Dog("Yorkie", 8, 20, 2, 4, 1, 20, "long silky");
+//        dog.eat();
+//        dog.walk();
+//        dog.run();
 
         //Exercise 34: Complex operations
 //        ComplexNumber one = new ComplexNumber(1.0, 1.0);
