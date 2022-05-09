23b7ebde20828cd25bd54263fb7579fda73b1d7e
brent-a <brent.k.aronsen@emcins.com>
2022-05-09 16:17:18 -0500

    Continue section 9 - Inner and abstract classes, interfaces

--- src/com/brentaronsen/Main.java
+++ src/com/brentaronsen/Main.java
@@ -1,5 +1,6 @@
 package com.brentaronsen;
 
+import com.brentaronsen.section9InnerAbstractInterfaces.LinkedListMain;
 import com.brentaronsen.section9InnerAbstractInterfaces.Section9Main;
 
 public class Main {
@@ -12,7 +13,8 @@ public class Main {
         //Section5Main.section5Main();
         //Section6Main.section6Main();
         //Section7Main.section7Main();
-        Section9Main.section9Main();
+        //Section9Main.section9Main();
+        LinkedListMain.main(args);
     }
 
 
--- src/com/brentaronsen/section9InnerAbstractInterfaces/Section9Main.java
+++ src/com/brentaronsen/section9InnerAbstractInterfaces/Section9Main.java
@@ -3,6 +3,15 @@ package com.brentaronsen.section9InnerAbstractInterfaces;
 public class Section9Main {
 
     public static void section9Main() {
+        ITelephone timsPhone;
+        timsPhone = new DeskPhone(123456);
+        timsPhone.powerOn();
+        timsPhone.callPhone(123456);
+        timsPhone.answer();
 
+        timsPhone = new MobilePhone(24565);
+        timsPhone.powerOn();
+        timsPhone.callPhone(24565);
+        timsPhone.answer();
     }
 }
